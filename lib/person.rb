c572b1c4a6a1f949b6481fa0caf6d9afbb43efdf
Teerth Patel <teerth1205@gmail.com>
2020-01-29 03:30:52 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,20 @@
+class Person
+  
+  def name=(p_name)
+    @name = p_name 
+  end 
+  
+  def name
+    @name
+  end
+  
+  def job=(jobs)
+    @job = jobs 
+  end 
+  
+  def job
+    @job
+  end 
+
+  
+end 
